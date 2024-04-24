e7080ce0d636e4dbe12cb1ac3542e83ef560a9c0
MarkZither <mark.burton@zither-it.co.uk>
2024-04-24 22:14:18 +0200

    check prf to set v3 shatype correctly

--- src/Hashcat.ASPNET.Identity/AspNetIdentityHashInfo.cs
+++ src/Hashcat.ASPNET.Identity/AspNetIdentityHashInfo.cs
@@ -1,10 +1,33 @@
 ﻿using System.Globalization;
+using System.Security.Cryptography;
+
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
 using NetDevPack.Utilities;
 
 namespace Hashcat.ASPNET.Identity;
 
 public class AspNetIdentityHashInfo
 {
+    /* =======================
+     * HASHED PASSWORD FORMATS - https://github.com/dotnet/aspnetcore/blob/main/src/Identity/Extensions.Core/src/PasswordHasher.cs
+     * =======================
+     *
+     * Version 2:
+     * PBKDF2 with HMAC-SHA1, 128-bit salt, 256-bit subkey, 1000 iterations.
+     * (See also: SDL crypto guidelines v5.1, Part III)
+     * Format: { 0x00, salt, subkey }
+     *
+     * Version 3 (.net 6 and below):
+     * PBKDF2 with HMAC-SHA256, 128-bit salt, 256-bit subkey, 10000 iterations.
+     * Format: { 0x01, prf (UInt32), iter count (UInt32), salt length (UInt32), salt, subkey }
+     * (All UInt32s are stored big-endian.)
+     * Version 3 (.net 7 and above):
+     * PBKDF2 with HMAC-SHA512, 128-bit salt, 256-bit subkey, 100000 iterations.
+     * Format: { 0x01, prf (UInt32), iter count (UInt32), salt length (UInt32), salt, subkey }
+     * (All UInt32s are stored big-endian.)
+
+     */
     public AspNetIdentityHashInfo(string base64Hash){
         HexHash = base64Hash.FromBase64().ToPlainHexDumpStyle();
         Hash = base64Hash;
@@ -41,12 +64,31 @@ public class AspNetIdentityHashInfo
         HexSaltLength = HexHash.Substring(18, 8);
         HexSalt = HexHash.Substring(26, 32);
         HexSubKey = HexHash.Substring(58, 64);
+        var prf = int.Parse(HexPrf, NumberStyles.HexNumber);
         IterCount = int.Parse(HexIterCount, NumberStyles.HexNumber);
         SaltLength = int.Parse(HexSaltLength, NumberStyles.HexNumber) * 8;
         Salt = HexSalt.FromPlainHexDumpStyleToByteArray().ToBase64();
         SubKey = HexSubKey.FromPlainHexDumpStyleToByteArray().ToBase64();
         HashcatFormat = $"sha256:{IterCount}:{Salt}:{SubKey}";
-        ShaType = "sha256";
+        ShaType = GetShaTypeForPrf(prf);
+    }
+
+    private string GetShaTypeForPrf(int prf) {
+        HashAlgorithmName algorithmName;
+        switch (prf) {
+            case (int)KeyDerivationPrf.HMACSHA1:
+                algorithmName = HashAlgorithmName.SHA1;
+                break;
+            case (int)KeyDerivationPrf.HMACSHA256:
+                algorithmName = HashAlgorithmName.SHA256;
+                break;
+            case (int)KeyDerivationPrf.HMACSHA512:
+                algorithmName = HashAlgorithmName.SHA512;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(prf));
+        }
+        return algorithmName.Name;
     }
 
     public string ShaType { get; set; }
