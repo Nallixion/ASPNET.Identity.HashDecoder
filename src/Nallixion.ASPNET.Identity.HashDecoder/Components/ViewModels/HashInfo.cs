0bcf20b92dc1c1f4badab0ae16b7918dc8eff3f2
MarkZither <mark.burton@zither-it.co.uk>
2024-05-26 21:47:33 +0200

    Create decode blazor page

---
+++ src/Nallixion.ASPNET.Identity.HashDecoder/Components/ViewModels/HashInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace Nallixion.ASPNET.Identity.HashDecoder.Components.ViewModels {
+    public class HashInfo {
+        public string PasswordHash { get; set; } = string.Empty;
+        public string ShaType { get; set; } = string.Empty;
+
+        public string HashcatFormat { get; set; } = string.Empty;
+
+        public string SubKey { get; set; } = string.Empty;
+
+        public string Salt { get; set; } = string.Empty;
+
+        public int SaltLength { get; set; }
+
+        public int IterCount { get; set; }
+
+        public string HexSubKey { get; set; } = string.Empty;
+
+        public string HexSalt { get; set; } = string.Empty;
+
+        public string HexSaltLength { get; set; } = string.Empty;
+
+        public string HexIterCount { get; set; } = string.Empty;
+
+        public string HexPrf { get; set; } = string.Empty;
+
+        public string HashVersion { get; set; } = string.Empty;
+        public string HexHash { get; set; } = string.Empty;
+        public string Hash { get; set; } = string.Empty;
+    }
+}
